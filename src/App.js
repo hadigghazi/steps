31e559e2e6c429c78b868a9be2f9cfa49dd171a5
hadigghazi <128805519+hadigghazi@users.noreply.github.com>
2023-12-31 08:08:54 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep((s) => s + 1);
+  }
+  return (
+    <>
+      <button
+        className="close"
+        onClick={() => {
+          isOpen ? setIsOpen(false) : setIsOpen(true);
+        }}
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <Button
+              backgroundColor="#7950f2"
+              color="#fff"
+              onClick={handlePrevious}
+            >
+              <span>👈</span> Previous
+            </Button>
+            <Button backgroundColor="#7950f2" color="#fff" onClick={handleNext}>
+              Next <span>👉</span>{" "}
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+function Button({ backgroundColor, color, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: backgroundColor, color: color }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default App;
